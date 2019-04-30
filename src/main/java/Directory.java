55990a79ccdf4b95e74171836bb15694eb4cb0ee
Vlad1m1ru5 <kubantsev.vov@mail.ru>
2019-04-30 21:19:20 +0300

    Открытие директории. Доделать фиксацию выбранного пути файла.

--- src/main/java/Directory.java
+++ src/main/java/Directory.java
@@ -4,13 +4,29 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 
-public class Directory {
-    private String path = "";
-    private final String logFile = "SysLog.txt";
+class Directory {
+    private String path;
     private boolean fixed;
+
+    private JFrame dialogFrame;
+    private JButton searchBtn;
+    private JButton finishBtn;
+    private JTextField pathFld;
+    private JPanel panel;
+    private JPanel panelButtons;
+    private JFileChooser dirChooser;
 
     Directory() {
         fixed = false;
+        path = "";
+
+        dialogFrame = new JFrame("myDirectory");
+        searchBtn = new JButton("Search");
+        finishBtn = new JButton("Finish");
+        pathFld = new JTextField("", 125);
+        panel = new JPanel();
+        panelButtons = new JPanel();
+        dirChooser = new JFileChooser();
     }
 
     public boolean isFixed() {
@@ -23,38 +39,12 @@ public class Directory {
     }
 
     public void selectDir() {
-        JFrame dialogFrame = new JFrame("myDirectory");
-        JButton searchBtn = new JButton("Search");
-        JButton finishBtn = new JButton("Finish");
-        final JTextField pathFld = new JTextField("", 170);
-        JPanel panel = new JPanel();
-        JPanel panelButtons = new JPanel();
-        final JFileChooser dirChooser = new JFileChooser();
-
-        dialogFrame.setPreferredSize(new Dimension(300, 125));
-        dialogFrame.setMinimumSize(new Dimension(300, 125));
-        dialogFrame.setMinimumSize(new Dimension(300, 125));
+        dialogFrame.setPreferredSize(new Dimension(300, 90));
+        dialogFrame.setMinimumSize(new Dimension(300, 90));
+        dialogFrame.setMinimumSize(new Dimension(300, 90));
         dialogFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        searchBtn.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                dirChooser.setName("myDirectoryNavigator");
-                dirChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-                final File selectedFile = dirChooser.getSelectedFile();
-                path = selectedFile.getAbsoluteFile().toString();
-                pathFld.setText(path);
-            }
-        });
-
-        finishBtn.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (!path.equals("")) {
-                    System.exit(0);
-                } else {
-                    pathFld.setText("Укажите перед выходом путь к файлу.");
-                }
-            }
-        });
+        fileChooserListener();
 
         panelButtons.setLayout(new FlowLayout());
         panelButtons.add(searchBtn);
@@ -69,5 +59,28 @@ public class Directory {
         dialogFrame.add(panel);
         dialogFrame.setEnabled(true);
         dialogFrame.setVisible(true);
+    }
+
+    private void fileChooserListener() {
+        searchBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                dirChooser.setName("myDirectoryNavigator");
+                dirChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                final File selectedFile = dirChooser.getCurrentDirectory();
+                path = selectedFile.getAbsolutePath().toString();
+                pathFld.setText(path);
+            }
+        });
+
+        finishBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (!path.equals("")) {
+                    fixed = true;
+                    System.exit(0);
+                } else {
+                    pathFld.setText("Укажите перед выходом путь к файлу.");
+                }
+            }
+        });
     }
 }
